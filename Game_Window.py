e27f03ca7dd1af3b70adfdcd95cc8ca245adb86c
Cayden <cha24075@byui.edu>
2026-03-16 20:46:35 -0600

    got jumping and directional changes working

--- Game_Window.py
+++ Game_Window.py
@@ -10,13 +10,24 @@ PLAYER_X = GAME_WIDTH / 2
 PLAYER_Y = GAME_HEIGHT / 2
 PLAYER_WIDTH = 108
 PLAYER_HEIGHT = 108
+PLAYER_JUMP_WIDTH = 52
+PLAYER_JUMP_HEIGHT = 60
 PLAYER_DISTANCE = 5
 
+GRAVITY = 0.5
+PlAYER_VELOCITY_Y = -10
+FLOOR_Y = GAME_HEIGHT * 3/4
+
 #images
-background_image = pygame.image.load(os.path.join("images", "temp_background.png")) # load the background image
-background_image = pygame.transform.scale(background_image, (GAME_WIDTH, GAME_HEIGHT))
-player_image_right = pygame.image.load(os.path.join("images", "horse_right.png")) # load the player image
-player_image_right = (pygame.transform.scale(player_image_right, (PLAYER_WIDTH, PLAYER_HEIGHT))) # scale the player image to the player's width and height
+def load_image(image_name, scale=None):
+    image = pygame.image.load(os.path.join("images", image_name))
+    if scale is not None:
+        image = pygame.transform.scale(image, scale)
+    return image
+
+background_image = load_image("temp_background.png", (GAME_WIDTH, GAME_HEIGHT))
+player_image_right = load_image("Horse_Right.png", (PLAYER_WIDTH, PLAYER_HEIGHT))
+player_image_left = load_image("Horse_Left.png", (PLAYER_WIDTH, PLAYER_HEIGHT))
 
 
 pygame.init()  # always needed to initialize py game
@@ -30,15 +41,35 @@ class Player(pygame.Rect):
     def __init__(self):
         super().__init__(PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT)
         self.image = player_image_right
+        self.velocity_y = 0
+        self.direction = "right"
+        self.jumping = False
+
+    def update_image(self):
+        if self.direction == "right":
+            self.image = player_image_right
+        if self.direction == "left":
+            self.image = player_image_left
+
 
 # left (x) and top (y), width and height
 player = Player()
+
+def move():
+    player.y += player.velocity_y # update the player's vertical position based on its velocity
+    player.velocity_y += GRAVITY # apply gravity to the player's vertical velocity
+
+    if player.y + player.height > FLOOR_Y:
+        player.y = FLOOR_Y - player.height
+        player.velocity_y = 0 # Stops falling when hit the floor
+        player.jumping = False
 
 
 def draw():
     # window.fill('blue') # fill the window with blue color
     window.fill((0,0,0)) # fill the window with a custom color using RGB values
     window.blit(background_image, (0,0)) # draw the background image on the window at position (0,80)
+    player.update_image()
     window.blit(player.image, player) # draw the player image on the window at the player's position
 
 while True: #game loop
@@ -46,30 +77,24 @@ while True: #game loop
         if event.type == pygame.QUIT: #user clicks the x button in window
             pygame.quit()
             exit()
-    
-        # KEYDOWN - key was pressed, KEYUP - key was pressed/released
-        '''if event.type == pygame.KEYDOWN: # user presses a key
-            if event.key in (pygame.K_UP, pygame.K_a): # left arrow key or 'a' key
-                player.x -= 10 # move the player left by 10 pixels
-            if event.key in (pygame.K_RIGHT, pygame.K_d): # right arrow key or 'd' key
-                player.x += 10 # move the player right by 10 pixels
-            if event.key in (pygame.K_UP, pygame.K_w): # up arrow key or 'w' key
-                player.y -= 10 # move the player up by 10 pixels
-            if event.key in (pygame.K_DOWN, pygame.K_s): # down arrow key or 's' key
-                player.y += 10 # move the player down by 10 pixels'''
 
     keys = pygame.key.get_pressed() # get the state of all keyboard buttons
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
         player.x = max(player.x - PLAYER_DISTANCE, 0) # move the player left by 5 pixels, but not beyond the left edge of the window 
-        
+        player.direction = "left"
+
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         player.x = min(player.x + PLAYER_DISTANCE, GAME_WIDTH - player.width) # move the player right by 5 pixels, but not beyond the right edge of the window
-        
-    if keys[pygame.K_UP] or keys[pygame.K_w]:
-        player.y = max(player.y - PLAYER_DISTANCE, 0) # move the player up by 5 pixels, but not above the top of the window
+        player.direction = "right"
 
-    if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        player.y = min(player.y + PLAYER_DISTANCE, GAME_HEIGHT - player.height) # move the player down by 5 pixels, but not below the bottom of the window
+    if (keys[pygame.K_UP] or keys[pygame.K_w]) and not player.jumping:
+        player.velocity_y = PlAYER_VELOCITY_Y # set the player's vertical velocity to the defined value (jumping)
+        player.jumping = True
+
+    # if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+    #     player.y = min(player.y + PLAYER_DISTANCE, GAME_HEIGHT - player.height) # move the player down by 5 pixels, but not below the bottom of the window
+
+    move()
 
     draw()
     pygame.display.update()
